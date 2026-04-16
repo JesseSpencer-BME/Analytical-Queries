d09c9453ed6e54c798b6519cc630ea3ee5e19192
JesseSpencer-BME <jessespencer@benefitsme.com>
2026-04-15 23:56:26 -0500

    Add SQL query for Doug's full order history

---
+++ full_order_history_for_doug_v1.sql
@@ -0,0 +1,76 @@
+select   
+  concat(c.firstname,' ',c.lastname) as customer_name,
+  a.date_created as agreement_created,
+  c.employer_name as partner,
+  c.employer_detail_name,
+  c.salary,
+  c.employment_schedule as ft_pt,
+  TIMESTAMPDIFF(MONTH, c.start_date, NOW()) as tenure_months,  
+  cc.clarity_score,
+  a.magento_order_number,
+  a.status as agreement_status,
+  c.credit_limit as spending_limit,
+  c.credit_limit - balances.current_balance as spending_limit_remaining,
+  a.total as purchase_amount, 
+  a.term as term,
+  a.payments as payment_amt,
+  c.first_order,
+  c.total_orders-1 as number_prior_purchases,
+  date(c.created_at) as registered_date,
+  c.customer_score as bme_score,
+  balances.current_balance,
+  a.id as agreement_id,
+  c.entity_id as customer_id,
+  e.scoring_model_type,
+  csu.salary as salary_self_reported,
+  csu.tenure as tenure_self_reported,
+  csu.employer_name as employer_self_reported,
+  initial_payments.initial_payment,
+  order_sequence.cust_order_sequence,
+  last_payment_received as last_payment_received_date,
+  DATEDIFF(CURDATE(), last_payment_received) AS days_since_payment
+from financials.v_customer_entity_summary c
+  inner join bme.agreements a on c.entity_id = a.customer_id
+  left join financials.v_scoring_clarity_score cc on c.entity_id = cc.customer_id
+  left join bme.employer e on c.employer_id = e.employer_id
+  left join financials.customer_supplemental cs on c.entity_id = cs.customer_id
+  left join bme.customer_signups csu on cs.customer_signup_id = csu.id
+  left join financials.v_ledger_agreement_summary las on a.id = las.agreement_id
+  left join (
+  select
+  customer_id,
+  c.email,
+  sum(current_balance) as current_balance,
+  count(1)
+from
+  financials.v_ledger_agreement_summary las
+  inner join bme.agreements a on las.agreement_id = a.id
+  inner join bme.customer_entity c on a.customer_id = c.entity_id
+group by
+  customer_id  
+  ) balances
+on a.customer_id = balances.customer_id  
+left join (
+    select agreement_id, round(sum(amount),2) as initial_payment from bme.ledger
+  where status = 'initial_payment'
+  and cancelled_at is null
+  group by agreement_id  
+  ) initial_payments
+  on a.id = initial_payments.agreement_id
+
+  left join (
+  SELECT
+  customer_id,
+  id,
+  ROW_NUMBER() OVER (
+    PARTITION BY
+      customer_id
+    ORDER BY
+      id
+  ) AS cust_order_sequence
+FROM
+  bme.agreements   
+  ) order_sequence
+on a.id = order_sequence.id  
+-- where date(a.date_created) >= CURRENT_DATE - INTERVAL 7 DAY
+order by a.id desc
