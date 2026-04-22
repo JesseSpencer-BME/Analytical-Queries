012dee0032dadd254132eed08b5222a9b3c67c64
JesseSpencer-BME <jessespencer@benefitsme.com>
2026-04-22 16:50:44 -0500

    Add past due amounts to ledger analysis query

--- Paychex Monitoring/Paychex Ledger Analysis.sql
+++ Paychex Monitoring/Paychex Ledger Analysis.sql
@@ -13,7 +13,9 @@ select
   lsa.ledger_stage_category,
   sed.sub_employer_name,
   sed.sub_employer_status,
-  em.worker_id
+  em.worker_id,
+  a.past_due_amount,
+  apd.amount as past_due_agreement_amount
 from
   bme.ledger l
   left join bme.agreements a on l.agreement_id = a.id
@@ -22,6 +24,7 @@ from
   left join bme.employer e on c.employer_id = e.employer_id
   left join financials.v_ledger_status_attributes lsa on l.status = lsa.status
   left join financials.v_employer_subemployer_detail sed on em.employer_id = sed.employer_id and em.company_code = sed.company_code
+  left join bme.agreements_past_due apd on l.agreement_id = apd.agreement_id
 
   -- Get the next schedule date  
   left join (
@@ -53,7 +56,8 @@ select core.*,
     when past_due_employees.employee_manifest_id is not null then 'Employee past-due'
     else 'Low Risk'    
   end as risk_bucket,
-  case when status = 'past_due' then amount end as past_due_amt,
-  case when status = 'paid_payroll_deduction' then amount end as paid_payroll_deduction_amt  
+  case when status = 'past_due' then -amount end as past_due_amt,
+  case when status = 'paid_payroll_deduction' then -amount end as paid_payroll_deduction_amt,
+  case when status in ('past_due','paid_payroll_deduction') then -amount end as total_deduction_requested
 from  core 
 left join past_due_employees on core.employee_manifest_id = past_due_employees.employee_manifest_id
